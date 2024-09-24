ca33cd096f3bf0d1c5d16249d1b11edce572fc13
MdShijanAli <freelancershijan@gmail.com>
2024-09-24 23:40:15 +0600

    Update
    Barchart API Update

--- models/costModel.js
+++ models/costModel.js
@@ -260,12 +260,6 @@ function costModel() {
     try {
       collection = await getCollection();
   
-      // Define an array of month names
-      const monthNames = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-      ];
-  
       // Array to hold results
       const resultData = [];
   
@@ -298,7 +292,7 @@ function costModel() {
         const totalMoney = result.length > 0 ? result[0].totalMoney : 0;
   
         // Push the month and money into the resultData array
-        resultData.push({[monthNames[month - 1]]: totalMoney });
+        resultData.push(totalMoney);
       }
   
       return resultData;
